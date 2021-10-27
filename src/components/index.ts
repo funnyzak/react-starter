09900b6169e89a2bd12198482d27cb91d9e199e3
Leon <silenceace@gmail.com>
2021-10-27 14:32:37 +0800

    chore

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,2 @@
-export { default as Hello } from './Hello/3index';
-export { default as Image } from './Image/3index';
+export { default as Hello } from './Hello/index';
+export { default as Image } from './Image/index';
